e40cf60d288541f53517567c915313e66e6b457d
Antonis Papaioannou <mrantonp@gmail.com>
2024-04-27 12:12:13 +0300

    finish app

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -93,9 +93,9 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
           emptyTitle="No events found"
           emptyStateSubtext="Come back later for more events"
           collectionType="All Events"
-          limit={6}
-          page={1}
-          totalPages={2}
+          limit={3}
+          page={searchParams.page as string}
+          totalPages={relatedEvents?.totalPages}
         />
       </section>
     </>
